3e957a0024478810f6036aee72c57aae96979b4c
bhuwanjungthapa <bhuwanjung115@gmail.com>
2022-06-07 21:00:55 +0545

    login form bootstrap

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -72,10 +72,10 @@
             <!-- /.social-auth-links -->
 
             <p class="mb-1">
-                <a href="forgot-password.html">I forgot my password</a>
+                <a href="forgot-password.html">I forgot my password!</a>
             </p>
             <p class="mb-0">
-                <a href="register.html" class="text-center">Register a new membership</a>
+                <a href="register.html" class="text-center">Register a new membership!</a>
             </p>
         </div>
         <!-- /.login-card-body -->
