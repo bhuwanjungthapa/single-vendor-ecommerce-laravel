4c98e315bb51525a067ce8b6c243aca02a1c98ff
bhuwanjungthapa <bhuwanjung115@gmail.com>
2022-06-27 22:20:51 +0545

    include setup for product 0.5

--- resources/views/backend/product/includes/attribute.blade.php
+++ resources/views/backend/product/includes/attribute.blade.php
@@ -1,20 +1,21 @@
-<table class="table table-striped table-bordered" id="image_wrapper">
+<table class="table table-striped table-bordered" id="attribute_wrapper">
     <tr>
-        <th>Image</th>
-        <th>Title</th>
+        <th>Attribute</th>
+        <th>Value</th>
         <th>Action</th>
     </tr>
     <tr>
         <td>
-            {!!  Form::file('image_file[]', null,['class' => 'form-control'])!!}
+            {!! Form::select('attribute_id[]',$data['attributes'],null,['class' => 'form-control','placeholder' => "Select Attribute"]) !!}
         </td>
-        <td><input type="text" name="image_title[]" class="form-control" placeholder="Enter Image Title"/></td>
+        <td><input type="text" name="attribute_value[]" class="form-control" placeholder="Enter Attribute Value"/></td>
         <td>
             <a class="btn btn-block btn-warning sa-warning remove_row "><i class="fa fa-trash"></i></a>
         </td>
     </tr>
+
 </table>
-<button class="btn btn-info" type="button" id="addMoreImage" style="margin-bottom: 20px">
+<button class="btn btn-info" type="button" id="addMoreAttribute" style="margin-bottom: 20px">
     <i class="fa fa-plus"></i>
     Add
 </button>
